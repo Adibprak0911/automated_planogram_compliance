adbefb256707ad412c2a375a031921e01158f638
Aditya Prakash <adityabprak0911@gmail.com>
2024-07-08 15:04:39 +0530

    change 1

--- image_processing_testing/connecting_database/test.py
+++
@@ -1,6 +0,0 @@
-first_list = first_list = ["egg", "chocolateflavour", "newspaper", "mouse", "monitor", "phone", "charger"]
-second_list = ["chocolate", "vanilla", "caramel"]
-
-matches = {sub for item in first_list for sub in second_list if sub in item}
-
-print(matches)
